776f81c8ac1b91cd63d4934623e74fd3ecdd80dd
phoenixbox <shane@quickleft.com>
2013-11-18 16:43:11 -0700

    unwind segue set up on modal

--- ToDoList/XYZToDoListViewController.m
+++ ToDoList/XYZToDoListViewController.m
@@ -14,6 +14,11 @@
 
 @implementation XYZToDoListViewController
 
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue
+{
+    
+}
+
 - (id)initWithStyle:(UITableViewStyle)style
 {
     self = [super initWithStyle:style];
